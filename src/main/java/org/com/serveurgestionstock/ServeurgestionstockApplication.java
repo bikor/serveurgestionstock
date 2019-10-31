bdf4b25220a8e0ee534864fe67c6e0f2122f927f
bikor <bikorkoffi@gmail.com>
2019-10-31 11:34:00 +0000

    first commit

---
+++ src/main/java/org/com/serveurgestionstock/ServeurgestionstockApplication.java
@@ -0,0 +1,13 @@
+package org.com.serveurgestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurgestionstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurgestionstockApplication.class, args);
+    }
+
+}
